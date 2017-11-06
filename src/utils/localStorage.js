aa854eee98bf87a9dfe90b78c250a2f968766806
Miroslav Kubus <MiroslavK@kentico.com>
2017-11-06 16:12:00 +0100

    KA-208 Store of application data improved - not storing edit mode

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -2,7 +2,7 @@ export const saveNotesData = (state) => {
   const notes = state.notes;
 
   try {
-    const serializedNotes = JSON.stringify(notes);
+    const serializedNotes = JSON.stringify(prepareNotesForStoring(notes));
     localStorage.setItem('notes', serializedNotes);
   }
   catch (err) {
@@ -18,10 +18,31 @@ export const getSavedNotes = () => {
       return undefined;
     }
 
-    return JSON.parse(serializedNotes);
+    return prepareNotesForApplication(JSON.parse(serializedNotes));
   }
   catch (err) {
-    console.error('Error while retrieving data from local storage!');
+    console.error('Error while retrieving data from local storage!' + err.message);
     return undefined;
   }
+};
+
+const prepareNotesForStoring = (notes) => {
+  return notes
+    .valueSeq()
+    .map(note => ({
+      text: note.text,
+      uid: note.uid,
+    }));
+};
+
+const prepareNotesForApplication = (notes) => {
+  return notes
+    .map(note => {
+      return [note.uid,
+        {
+          ...note,
+          isEditActive: false,
+        },
+      ];
+    });
 };
