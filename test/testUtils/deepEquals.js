33eeaaecea23e45d522bee89b043f03bf03c2102
Miroslav Kubus <MiroslavK@kentico.com>
2017-11-14 17:45:46 +0100

    KA-208 Downgrade immutableJS version to 3.8.2 due to bugs
    Version 4.0.0-rc.9 is not working properly with immutable prop-types. It
    throws an error in prop-type validation for Record although the prop is
    an instance of immutable Record. This error disappeared after the downgrade
    to version 3.8.2.

--- test/testUtils/deepEquals.js
+++ test/testUtils/deepEquals.js
@@ -1,7 +1,7 @@
 import { Record } from 'immutable';
 
 export const deepNoteEqual = (expectedNotePayload, actualNote) => {
-  expect(Record.isRecord(actualNote)).toBeTruthy();
+  expect(actualNote).toBeInstanceOf(Record);
   expect(actualNote.text).toEqual(expectedNotePayload.text);
   expect(actualNote.id).toEqual(expectedNotePayload.id);
   expect(actualNote.isEditActive).toEqual(expectedNotePayload.isEditActive);
