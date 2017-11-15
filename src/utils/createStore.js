972682fcdef3d3aff1c44445a3a7b329a9bbfee0
Miroslav Kubus <MiroslavK@kentico.com>
2017-11-15 13:13:25 +0100

    KA-208 Refactor createStore

--- src/utils/createStore.js
+++ src/utils/createStore.js
@@ -8,32 +8,35 @@ import { OrderedMap } from 'immutable';
 import throttle from 'lodash/throttle';
 
 export const createApplicationStore = (getSavedNotes, saveNotesData) => {
-  const persistedNotes = getSavedNotes('notes');
-  let initialState = undefined;
-
-  if (persistedNotes !== undefined) {
-    initialState = {
-      notes: {
-        listOfNotes: OrderedMap(persistedNotes),
-        isAddingNote: false,
-      },
-    };
-  }
-
   const store = createStore(
     application,
-    initialState,
+    prepareInitialState(getSavedNotes),
     applyMiddleware(logger)
   );
 
-  store.subscribe(throttle(() => {
+  store.subscribe(saveData(saveNotesData, store));
+
+  return store;
+};
+
+const prepareInitialState = (getSavedNotes) => {
+  const persistedNotes = getSavedNotes('notes');
+
+  return persistedNotes && {
+    notes: {
+      listOfNotes: OrderedMap(persistedNotes),
+      isAddingNote: false,
+    },
+  };
+};
+
+const saveData = (saveNotesData, store) =>
+  throttle(() => {
     const stateNotes = store
       .getState()
       .notes
       .listOfNotes;
 
     saveNotesData('notes', stateNotes);
-  }, 1000));
+  }, 1000);
 
-  return store;
-};
