342d48cf2fbeb44635661e6799af90e8a5bf2e70
Miroslav Kubus <MiroslavK@kentico.com>
2017-11-21 13:38:54 +0100

    KA-208 Remove app dummy test

--- test/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from '../src/App.jsx';
-
-xit('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
