3228701ce409dd0220cdc23510a4827b9b89228e
Miroslav Kubus <MiroslavK@kentico.com>
2017-11-22 16:33:51 +0100

    KA-208 Refactor isAddingNote reducer and actions

--- src/actions/addListMemberActionCreators.js
+++ src/actions/addListMemberActionCreators.js
@@ -1,19 +1,13 @@
 import {
-  START_ADDING_NOTE,
-  STOP_ADDING_NOTE,
+  START_ADDING_NOTE_FOCUS,
+  STOP_ADDING_NOTE_FOCUS,
 } from '../constants/actionTypes';
 
 export const startAddingNote = () => ({
-  type: START_ADDING_NOTE,
-  payload: {
-    isAddListMemberFocused: true,
-  },
+  type: START_ADDING_NOTE_FOCUS,
 });
 
 export const stopAddingNote = () => ({
-  type: STOP_ADDING_NOTE,
-  payload: {
-    isAddListMemberFocused: false,
-  },
+  type: STOP_ADDING_NOTE_FOCUS,
 });
 
--- src/reducers/notes/list/isAddingNote.js
+++ src/reducers/notes/list/isAddingNote.js
@@ -1,14 +1,10 @@
 export const isAddingNote = (state = false, action) => {
   switch (action.type) {
-    case 'START_ADDING_NOTE':
-      return changeIsAddingNote(state, action);
-    case 'STOP_ADDING_NOTE':
-      return changeIsAddingNote(state, action);
+    case 'START_ADDING_NOTE_FOCUS':
+      return true;
+    case 'STOP_ADDING_NOTE_FOCUS':
+      return false;
     default:
       return state;
   }
 };
-
-const changeIsAddingNote = (state, action) =>
-  action.payload.isAddListMemberFocused;
-
