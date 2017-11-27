4f86bae9d89b087b9f12bd56748e51c1612cee46
Miroslav Kubus <MiroslavK@kentico.com>
2017-11-27 14:35:43 +0100

    KA-209 Refactor IAction

--- src/models/IAction.ts
+++ src/models/IAction.ts
@@ -1,12 +1,4 @@
-type ActionType = 'ADD_NOTE' |
-  'UPDATE_NOTE' |
-  'DELETE_NOTE' |
-  'START_EDITING_NOTE' |
-  'CANCEL_EDITING_NOTE' |
-  'ADD_NOTE_FOCUS' |
-  'ADD_NOTE_BLUR';
-
 export interface IAction {
-  type: ActionType;
+  type: string;
   payload?: any;
 };
