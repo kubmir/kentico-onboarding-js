7f0c979af9cf70a8af6858f69bd4f7b0ba0896a2
Miroslav Kubus <MiroslavK@kentico.com>
2017-11-30 11:37:12 +0100

    KA-209 Refactor listOfNotes help functions

--- src/reducers/notes/list/listOfNotes.ts
+++ src/reducers/notes/list/listOfNotes.ts
@@ -8,9 +8,10 @@ import {
   UPDATE_NOTE,
 } from '../../../constants/actionTypes';
 import { IAction } from '../../../models/IAction';
+import { Guid } from '../../../@types/globals';
 
-const setNoteToState = (state: OrderedMap<string, Note>, action: IAction): OrderedMap<string, Note> => {
-  const { noteId, text } = action.payload;
+const setNoteToState = (state: OrderedMap<string, Note>, payload: { noteId: Guid, text: string }): OrderedMap<string, Note> => {
+  const { noteId, text } = payload;
   const noteToAdd = new Note({
     id: noteId,
     text,
@@ -22,8 +23,8 @@ const setNoteToState = (state: OrderedMap<string, Note>, action: IAction): Order
   return newNotes;
 };
 
-const updateText = (state: OrderedMap<string, Note>, action: IAction): OrderedMap<string, Note> => {
-  const { noteId, text } = action.payload;
+const updateText = (state: OrderedMap<string, Note>, payload: { noteId: Guid, text: string }): OrderedMap<string, Note> => {
+  const { noteId, text } = payload;
 
   return state.update(noteId, note => note.with({
     text,
@@ -31,15 +32,15 @@ const updateText = (state: OrderedMap<string, Note>, action: IAction): OrderedMa
   }));
 };
 
-const deleteNote = (state: OrderedMap<string, Note>, action: IAction): OrderedMap<string, Note> => {
+const deleteNote = (state: OrderedMap<string, Note>, payload: {noteId: Guid}): OrderedMap<string, Note> => {
   const currentNotes = state
-    .delete(action.payload.noteId);
+    .delete(payload.noteId);
 
   return currentNotes;
 };
 
-const updateEditingMode = (state: OrderedMap<string, Note>, action: IAction, newEditingMode: boolean): OrderedMap<string, Note> => {
-  const { noteId } = action.payload;
+const updateEditingMode = (state: OrderedMap<string, Note>, payload: {noteId: Guid}, newEditingMode: boolean): OrderedMap<string, Note> => {
+  const { noteId } = payload;
 
   return state.update(noteId, note => note.with({
     isEditActive: newEditingMode,
@@ -49,15 +50,15 @@ const updateEditingMode = (state: OrderedMap<string, Note>, action: IAction, new
 export const listOfNotes = (state = OrderedMap<string, Note>(), action: IAction): OrderedMap<string, Note> => {
   switch (action.type) {
     case ADD_NOTE:
-      return setNoteToState(state, action);
+      return setNoteToState(state, action.payload);
     case UPDATE_NOTE:
-      return updateText(state, action);
+      return updateText(state, action.payload);
     case DELETE_NOTE:
-      return deleteNote(state, action);
+      return deleteNote(state, action.payload);
     case START_EDITING_NOTE:
-      return updateEditingMode(state, action, true);
+      return updateEditingMode(state, action.payload, true);
     case CANCEL_EDITING_NOTE:
-      return updateEditingMode(state, action, false);
+      return updateEditingMode(state, action.payload, false);
     default:
       return state;
   }
