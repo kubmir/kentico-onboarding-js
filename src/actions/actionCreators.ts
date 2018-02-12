54b14fd66284dcbb681bf30d6970b345cdd77301
Miroslav Kubus <MiroslavK@kentico.com>
2018-02-12 16:05:14 +0100

    KA-270 Add action creators for adding note to server

--- src/actions/actionCreators.ts
+++ src/actions/actionCreators.ts
@@ -1,7 +1,4 @@
-import { generateId } from '../utils/generateId';
-import { addNewNoteFactory } from './notesActionCreators';
-
-export const addNewNote = addNewNoteFactory(generateId);
 export * from './addNoteActionCreators';
 export * from './notesActionCreators';
 export * from './serverActionCreators/getActionCreators';
+export * from './serverActionCreators/postActionCreators';
--- src/actions/addNoteActionCreators.ts
+++ src/actions/addNoteActionCreators.ts
@@ -2,8 +2,12 @@ import {
   ADD_NOTE_FOCUS,
   ADD_NOTE_BLUR,
   ADD_NOTE_TEXT_CHANGE,
+  START_SENDING_NOTE_TO_SERVER,
+  SENDING_NOTE_TO_SERVER_FAILURE,
+  SENDING_NOTE_TO_SERVER_SUCCESS,
 } from '../constants/actionTypes';
 import { IAction } from '../models/IAction';
+import { Note } from '../models/Note';
 
 export const startAddingNote = (): IAction => ({
   type: ADD_NOTE_FOCUS,
@@ -20,3 +24,21 @@ export const changeAddingNoteText = (newText: string): IAction => ({
   }
 });
 
+export const startSendingNoteToServer = (): IAction => ({
+  type: START_SENDING_NOTE_TO_SERVER,
+});
+
+export const sendingNoteToServerFailed = (errorDescription: string): IAction => ({
+  type: SENDING_NOTE_TO_SERVER_FAILURE,
+  payload: {
+    errorDescription,
+  }
+});
+
+export const sendingNoteToServerSuccess = (addedNote: Note): IAction => ({
+  type: SENDING_NOTE_TO_SERVER_SUCCESS,
+  payload: {
+    noteId: addedNote.id,
+    text: addedNote.text,
+  }
+});
