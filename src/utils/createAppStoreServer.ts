4305b09dfd949971c7fa8052519acaad18a6b20b
Miroslav Kubus <MiroslavK@kentico.com>
2018-02-14 16:06:10 +0100

    KA-270 Use compose in createStore for ability to use redux dev-tools

--- src/utils/createAppStoreServer.ts
+++ src/utils/createAppStoreServer.ts
@@ -2,6 +2,7 @@ import { application } from '../reducers/application';
 import { IStoreState } from '../models/IStoreState';
 import {
   applyMiddleware,
+  compose,
   createStore
 } from 'redux';
 import thunk from 'redux-thunk';
@@ -9,11 +10,13 @@ import { logger } from 'redux-logger';
 import { Store } from 'react-redux';
 import { getAllNotes } from '../actions/actionCreators';
 
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export const createAppStoreServer = (): Store<IStoreState> => {
 
   const store = createStore<IStoreState>(
     application,
-    applyMiddleware(logger, thunk)
+    composeEnhancers(applyMiddleware(logger, thunk)),
   );
 
   store.dispatch(getAllNotes);
